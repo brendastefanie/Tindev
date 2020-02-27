802979bbd3cab9208ca3afd4625bd7da120236f0
B. Oliveira Saldanha <b.oliveira.saldanha@avanade.com>
2020-02-26 21:08:31 -0300

    connection to the database

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -6,4 +6,8 @@ routes.get('/', (req, res) => {
     res.json({ message: `Hello ${req.query.name}` })
 })
 
+routes.post('/devs', (req, res) => {
+    res.json(req.body)
+})
+
 module.exports = routes
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,16 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const routes = require('./routes')
 
 const server = express()
 
+mongoose.connect('mongodb+srv://<username>:<password>@cluster0-mfkqc.mongodb.net/Tindev?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+server.use(express.json())
 server.use(routes)
 
 server.listen(8080)
